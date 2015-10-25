e5cf5380f15d6afcaf3802918e4cb53882031323
wolforce <wolforced@gmail.com>
2015-10-25 23:14:20 +0800

    - 添加Sublime Text 3的支持.

--- StockRepoter/Stockrepoter.py
+++ StockRepoter/Stockrepoter.py
@@ -1,9 +1,15 @@
 #coding=utf-8
 import sublime, sublime_plugin
-import urllib2
 import threading
 import re
-from xpinyin import Pinyin
+
+try:
+    import urllib2
+    from xpinyin import Pinyin
+    isPython2 = True
+except ImportError:
+    import urllib.request as urllib2
+    isPython2 = False
 
 status_key = "StockInfo"
 
@@ -40,10 +46,10 @@ class FetchStocksCall(threading.Thread):
         threading.Thread.__init__(self)
 
     def get_stock_with_prefix(self, no):
-        sh = [001, 110, 120, 129, 100, 201, 310, 500, 550, 600, 700, 710, 701, 711, 720, 730, 735, 737, 900]
+        sh = ['001', '110', '120', '129', '100', '201', '310', '500', '550', '600', '700', '710', '701', '711', '720', '730', '735', '737', '900']
         prefix = 'sz'
         for num in sh:
-            if no.startswith(str(num)):
+            if no.startswith(num):
                 prefix = 'sh'
                 break
         return prefix + no
@@ -60,9 +66,17 @@ class FetchStocksCall(threading.Thread):
             rstring += unichr(inside_code)
         return rstring
 
+    #因为sublime2不支持在状态栏显示中文，所以在python2下用拼音表示股票名，在python3下直接使用中文.
+    def get_stock_name(self, name):
+        stockName = ""
+        if isPython2:
+            stockName = Pinyin().get_initials(self.strQ2B(name), u'')
+        else:
+            stockName = name
+        return stockName
+
     def run(self):
         try:
-            p = Pinyin()
             for stock in self.stocks:
                 no       = stock.get('no')
                 request  = urllib2.Request('http://hq.sinajs.cn/list=' + self.get_stock_with_prefix(no))
@@ -70,16 +84,16 @@ class FetchStocksCall(threading.Thread):
                 result   = response.read().decode('gbk')
                 text     = re.sub('var.+=[^"]*"', '', result)
                 infos    = text.split(',')
-                stockName      = p.get_initials(self.strQ2B(infos[0]), u'')
+                stockName      = self.get_stock_name(infos[0])
                 yesterdayPrice = float(infos[2])
                 curPrice       = float(infos[3])
-                percent        = (curPrice / yesterdayPrice - 1) * 100,
+                percent        = (curPrice / yesterdayPrice - 1) * 100
                 percentStr     = str('%0.2f'%percent) + "%"
                 self.priceList.append({'no':no, 'name':stockName, 'percent': percentStr})
             return
-        except (urllib2.HTTPError) as (e):
-            err = '%s: HTTP error %s contacting API' % (__name__, str(e.code))
-        except (urllib2.URLError) as (e):
-            err = '%s: URL error %s contacting API' % (__name__, str(e.reason))
+        except urllib2.HTTPError:
+            err = '%s: HTTP error %s contacting API' % (__name__, str(urllib2.HTTPError.code))
+        except urllib2.URLError:
+            err = '%s: URL error %s contacting API' % (__name__, str(urllib2.URLError.reason))
 
         sublime.error_message(err)
